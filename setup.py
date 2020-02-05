b3b531ffb88a301f27d956b83682d5c61c5ccc03
zzossig <zzossig@gmail.com>
2020-02-05 11:08:32 +0900

    date format changed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = ['bibtexparser==1.1.0']
 
 setuptools.setup(
     name="zzo-bibtex-parser",
-    version="1.0.2",
+    version="1.0.3",
     license="WTFPL",
     author="zzossig",
     author_email="zzossig@gmail.com",
--- zzo/parser.py
+++ zzo/parser.py
@@ -3,11 +3,13 @@
 import sys
 import os
 import re
+import calendar
 import argparse
 from time import strptime
 import bibtexparser
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
+
 
 def main():
     parse(sys.argv[1:])
@@ -25,6 +27,9 @@ def parse(args):
             bib_database = bibtexparser.load(bibtex_file)
 
         for dic in bib_database.entries:
+            year = '0000'
+            month = '01'
+            day = '01'
             result = ['---']
 
             for key in dic:
@@ -41,16 +46,24 @@ def parse(args):
                         result.append(f'link: "{parsed_dict}"')
                     elif key == 'journal':
                         result.append(f'publication: "{parsed_dict}"')
+                    elif key == 'year':
+                        year = parsed_dict
+                    elif key == 'month':
+                        month = month_string_to_number(parsed_dict)
+                    elif key == 'day':
+                        day = parsed_dict
                     else:
                         doubleQuoteEscape = parsed_dict.replace('"', '\\"')
                         result.append(f'{key}: "{doubleQuoteEscape}"')
 
+            result.append(f'publishDate: "{year}-{month}-{day}"')
             result.append('---')
 
             # make file
             if dic['ID']:
                 # md file
-                filename = f"content/publication/{dic['ID']}/index.md"
+                renamedDir = re.sub(r'(?<!^)(?=[A-Z])', '_', dic['ID']).lower()
+                filename = f"content/publication/{renamedDir}/index.md"
                 dirname = os.path.dirname(filename)
                 if not os.path.exists(dirname):
                     os.makedirs(dirname)
@@ -61,11 +74,34 @@ def parse(args):
                 db = BibDatabase()
                 db.entries = [dic]
                 writer = BibTexWriter()
-                with open(f"content/publication/{dic['ID']}/cite.bib", 'w', encoding='utf8') as bibfile:
+                with open(f"content/publication/{renamedDir}/cite.bib", 'w', encoding='utf8') as bibfile:
                     bibfile.write(writer.write(db))
             else:
                 print('There is no ID')
 
+
+def month_string_to_number(string):
+    m = {
+        'jan': "01",
+        'feb': "02",
+        'mar': "03",
+        'apr': "04",
+        'may': "05",
+        'jun': "06",
+        'jul': "07",
+        'aug': "08",
+        'sep': "09",
+        'oct': "10",
+        'nov': "11",
+        'dec': "12"
+    }
+    s = string.strip()[:3].lower()
+
+    try:
+        out = m[s]
+        return out
+    except:
+        raise ValueError('Not a month')
 
 if __name__ == "__main__":
     main()
